f23d13ec3041131389c41c85005b29aa5c7c97cc
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-06 23:30:41 -0300

    Create script.js

---
+++ javascript-obc/des001/script.js
@@ -0,0 +1,10 @@
+// Calcular a Hipotenusa
+
+var a = 3;
+var b = 4;
+
+// window.prompt('Digite o valor cateto:')
+// window.prompt('Digite o outro cateto:')
+
+var c = Math.sqrt(Math.pow(a, 2) + Math.pow(b, 2))
+console.log(c)

d91cc9ace4bffa6395d6ad6c4bdc86ac427577f5
ODCS1 <119748470+ODCS1@users.noreply.github.com>
2023-02-26 13:54:36 -0300

    ex008 update

--- javascript-obc/exercicios/ex008/script.js
+++ javascript-obc/exercicios/ex008/script.js
@@ -1,21 +1,32 @@
 /*  Escreva um programa em javascript que peça o nome de um turista e então pergunte se ele já visitou alguma cidade. Caso a resposta seja sim, pergunte o nome da cidade e o armazene em uma variável, e então continue perguntando se o turista visitou alguma outra cidade até que a resposta seja não, No fim, o programa deve exibir o nome do turista, quantas e quais cidades ele visitou.
-*/
-const nome = prompt('Nome: ')
+ */
 
-let res = prompt('Você já visitou alguma cidade[s/n]?')
+const nome = prompt("Olá turista, qual é o seu nome: ");
 
-let qtd = 0
-if (res ==='s'){
-  qtd += 1
-  prompt('Qual o nome dessa cidade: ')
+let cidades = "";
+let qtd = 0;
+
+let res = prompt("Você já visitou alguma cidade[s/n]?").toLowerCase();
+
+if (res === "s") {
+  qtd += 1;
+  cidades += " " + prompt("Qual o nome dessa cidade: ");
+  do {
+    res = prompt(`${nome}, você já visitou outra cidade[s/n]?`).toLowerCase();
+    if (res === "s") {
+      qtd += 1;
+      cidades += ", " + prompt("Qual o nome dessa cidade: ");
+    }
+  } while (res == "s");
+}else if (res != 's' && res != 'n'){
+  alert('Escolha uma opção válida!')
+}
+
+if (cidades == "") {
+  alert("Boa sorte, em conhecer as cidades então!");
+} else {
+  alert(`${nome}, você visitou ${qtd} cidades já.`);
+  alert(`Cidades visitadas: ${cidades}.`);
 }
 
 
-do{
-  res = prompt(`${nome}, você já visitou outra cidade[s/n]?`).toLowerCase
-  if (res ==='s'){
-    qtd += 1
-  }
-}while(res == 's')
-
-alert(`${nome}, você visitou ${qtd} cidades já.`)
